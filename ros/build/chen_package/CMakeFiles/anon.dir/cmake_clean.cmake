6b70beb0223ed9f02aaf58902b1a53ac06db32ff
mi546870961 <chenyushen1989@163.com>
2017-11-20 17:15:35 +0800

    ros anon

---
+++ ros/build/chen_package/CMakeFiles/anon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/anon.dir/anon.cpp.o"
+  "/home/chenyushen/ros/devel/lib/chen_package/anon.pdb"
+  "/home/chenyushen/ros/devel/lib/chen_package/anon"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/anon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ros/build/chen_package/CMakeFiles/count.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/count.dir/count.cpp.o"
+  "/home/chenyushen/ros/devel/lib/chen_package/count.pdb"
+  "/home/chenyushen/ros/devel/lib/chen_package/count"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/count.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
