d628c4003db498de22c4b4849bc03bf48a8bfdf9
mi546870961 <chenyushen1989@163.com>
2017-11-23 16:49:02 +0800

    ros service

---
+++ ros/build/chen_package/CMakeFiles/spawn_turtle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/spawn_turtle.dir/spawn_turtle.cpp.o"
+  "/home/chenyushen/ros/devel/lib/chen_package/spawn_turtle.pdb"
+  "/home/chenyushen/ros/devel/lib/chen_package/spawn_turtle"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/spawn_turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
